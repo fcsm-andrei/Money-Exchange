ce6df9c0d9b392cb48e36ca92567d3aaac4a894c
fcsm-andrei <andreileonchikov@gmail.com>
2018-09-23 21:50:49 +0300

    Money-Exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	var obj = {};
+	var h = 50,
+		q = 25,
+		d = 10,
+		n = 5,
+		p = 1; 
+	if(currency <= 10000 && currency > 0) {
+		obj.H = Math.floor(currency / h);
+		obj.Q = Math.floor((currency % h) / q);
+		obj.D = Math.floor(((currency % h) % q) / d);
+		obj.N = Math.floor((((currency % h) % q) % d) / n);
+		obj.P = Math.floor((currency % n) / p);
+	} else if(currency > 10000) {
+		obj = {error: 'You are rich, my friend! We don\'t have so much coins for exchange'};
+	} else {
+		obj = {};
+	}
+	for (key in obj) {
+  		if(obj[key] === 0){
+  			delete obj[key];
+  		}
+	}  
+    return obj;
 }
+
+  //console.log(makeExchange(0));  //--> {}
+  //console.log(makeExchange(1));  //--> {"P":1}
+  //console.log(makeExchange(43)); //--> {"Q":1,"D":1,"N":1,"P":3}
+  //console.log(makeExchange(91)); //--> {"H":1,"Q":1,"D":1,"N":1,"P":1}
+  //console.log(makeExchange(9999999));  //--> {error: "You are rich, my friend! We don't have so much coins for exchange"}
